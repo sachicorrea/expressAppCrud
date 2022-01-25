6c01729cbd5c83c9a2db8a50b160f2a41e7cdfbd
sachic <scorrear@tutanota.com>
2022-01-25 08:24:59 -0500

    Login front connection

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,38 @@
+const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+// Connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectTimeout: 30000
+});
+
 exports.register = (req, res) => {
     console.log(req.body);
-    res.send("Form submitted");
+
+    const { name, email, password, passwordConfirm } = req.body;
+
+    connection.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+        console.log(results);
+        if (error) {
+            console.log(error);
+        }
+
+        if (results.length > 0) {
+            return res.render('register', {
+                message: 'El email ya está siendo utilizado'
+            })
+        } else if (password !== passwordConfirm) {
+            return res.render('register', {
+                message: 'Las contraseñas no coinciden'
+            })
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+    });
 }
